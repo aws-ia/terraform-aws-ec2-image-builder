59edadf1fc7610696a8b96561f600de6d54ca734
Maicon Alegre <alegrm@amazon.com>
2024-09-06 07:23:53 +0930

    add required_providers on provider to pass static analysis

--- examples/linux/providers.tf
+++ examples/linux/providers.tf
@@ -1,16 +1,20 @@
 terraform {
+  required_version = ">= 1.4.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
     }
-    awscc = {
+     awscc = {
       source  = "hashicorp/awscc"
+      version = ">= 0.24.0"
     }
     tls = {
       source  = "hashicorp/tls"
+      version = ">= 4.0.0, < 5.0.0"
     }
     random = {
       source  = "hashicorp/random"
+      version = ">= 3.0.0, < 4.0.0"
     }
   }
 }
