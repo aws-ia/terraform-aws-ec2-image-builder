882d515837b0e99158321af31ccedc1e12cd7af4
Maicon Alegre <alegrm@amazon.com>
2025-02-06 17:02:03 +1030

    update Readme and provider file

--- examples/linux/providers.tf
+++ examples/linux/providers.tf
@@ -1,13 +1,9 @@
 terraform {
-<<<<<<< HEAD
   required_version = ">= 1.4.0"
-=======
->>>>>>> 748f378 (add linux example + overall enhancements)
   required_providers {
     aws = {
       source  = "hashicorp/aws"
     }
-<<<<<<< HEAD
      awscc = {
       source  = "hashicorp/awscc"
       version = ">= 0.24.0"
@@ -19,16 +15,6 @@ terraform {
     random = {
       source  = "hashicorp/random"
       version = ">= 3.0.0, < 4.0.0"
-=======
-    awscc = {
-      source  = "hashicorp/awscc"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-    }
-    random = {
-      source  = "hashicorp/random"
->>>>>>> 748f378 (add linux example + overall enhancements)
     }
   }
 }
